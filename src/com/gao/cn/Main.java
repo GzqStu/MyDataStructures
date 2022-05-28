260e839040f75ba8852430a9db0a6bcbe9da400d
DESKTOP-0LUK01P\Mr.Gao <2538120593@qq.com>
2022-05-28 20:36:45 +0800

    测试栈和队列，数组的性能

--- src/com/gao/cn/Main.java
+++ src/com/gao/cn/Main.java
@@ -1,24 +1,84 @@
 package com.gao.cn;
 
 import com.gao.cn.array.Array;
+import com.gao.cn.queue.ArrayQueue;
+import com.gao.cn.queue.LoopQueue;
+import com.gao.cn.queue.Queue;
+import com.gao.cn.stack.ArrayStack;
+import com.gao.cn.stack.Stack;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        Array<Integer> arr = new Array<>();
-        for (int i = 0; i < 10; i++) {
-            arr.addLast(i);
+    /**
+     * 测试队列中opCount个数据增加和删除耗时
+     *
+     * @param queue
+     * @param opCount
+     * @return
+     */
+    private static Double testQueueTime(Queue<Integer> queue, int opCount) {
+        Long startTime = System.nanoTime();
+        for (int i = 0; i < opCount; i++) {
+            queue.enqueue(i);
         }
-        System.out.println(arr);
-        arr.add(1, 100);
-        System.out.println(arr);
-        arr.addLast(100);
-        arr.addLast(100);
-        System.out.println(arr);
-        arr.removeElement(100);
-        System.out.println(arr);
-        arr.removeElements(100);
-        System.out.println(arr);
+        for (int i = 0; i < opCount; i++) {
+            queue.deQueue();
+        }
+        Long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    /**
+     * 测试栈中opCount个数据增加和删除耗时
+     *
+     * @param stack
+     * @param opCount
+     * @return
+     */
+    private static Double testStackTime(Stack<Integer> stack, int opCount) {
+        Long startTime = System.nanoTime();
+        for (int i = 0; i < opCount; i++) {
+            stack.push(i);
+        }
+        for (int i = 0; i < opCount; i++) {
+            stack.pop();
+        }
+        Long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    /**
+     * 测试数组中opCount个数据增加和删除耗时
+     *
+     * @param array
+     * @param opCount
+     * @return
+     */
+    private static Double testArrayTime(Array<Integer> array, int opCount) {
+        Long startTime = System.nanoTime();
+        for (int i = 0; i < opCount; i++) {
+            array.addLast(i);
+        }
+        for (int i = 0; i < opCount; i++) {
+            array.removeLast();
+        }
+        Long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+        int opCount = 1000000;
+        //查询数组队列耗时
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        System.out.println("数组队列: " + testQueueTime(arrayQueue, opCount));
+        //查询循环队列耗时
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        System.out.println("循环队列: " + testQueueTime(loopQueue, opCount));
+        //查询栈耗时
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        System.out.println("栈: " + testStackTime(arrayStack, opCount));
+        //查询数组耗时
+        Array<Integer> array = new Array<>();
+        System.out.println("数组: " + testArrayTime(array, opCount));
     }
 }
--- src/com/gao/cn/array/Array.java
+++ src/com/gao/cn/array/Array.java
@@ -273,4 +273,21 @@ public class Array<E> {
         return res.toString();
     }
 
+    public static void main(String[] args) {
+        Array<Integer> arr = new Array<>();
+        for (int i = 0; i < 10; i++) {
+            arr.addLast(i);
+        }
+        System.out.println(arr);
+        arr.add(1, 100);
+        System.out.println(arr);
+        arr.addLast(100);
+        arr.addLast(100);
+        System.out.println(arr);
+        arr.removeElement(100);
+        System.out.println(arr);
+        arr.removeElements(100);
+        System.out.println(arr);
+    }
+
 }
