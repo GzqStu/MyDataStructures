ab5c92d0d4522759421659e1975735f660963704
DESKTOP-0LUK01P\Mr.Gao <2538120593@qq.com>
2022-05-29 17:19:14 +0800

    多种方式实现链表上删除元素

---
+++ src/com/gao/cn/leetCode/leetcode/editor/cn/ListNode.java
@@ -0,0 +1,46 @@
+package com.gao.cn.leetCode.leetcode.editor.cn;
+
+/**
+ * @author ：gaozhiqi
+ * @date ：2022/5/29 16:15
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public ListNode(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("arr can not be empty");
+        }
+        this.val = nums[0];
+        ListNode cur = this;
+        for (int i = 1; i < nums.length; i++) {
+            cur.next = new ListNode(nums[i]);
+            cur = cur.next;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder ret = new StringBuilder();
+        ListNode cur = this;
+        while (cur != null) {
+            ret.append(cur.val + "->");
+            cur = cur.next;
+        }
+        ret.append(" Null");
+        return ret.toString();
+    }
+}
