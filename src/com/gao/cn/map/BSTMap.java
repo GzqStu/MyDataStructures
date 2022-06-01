75f6ad92ec9955719d3f9dca7966cbc95847f16f
DESKTOP-0LUK01P\Mr.Gao <2538120593@qq.com>
2022-06-01 11:33:03 +0800

    基于二分搜索树和链表实现map映射

---
+++ src/com/gao/cn/map/BSTMap.java
@@ -0,0 +1,192 @@
+package com.gao.cn.map;
+
+/**
+ * 基于二分搜索树实现map
+ *
+ * @author ：gaozhiqi
+ * @date ：2022/6/1 10:47
+ */
+public class BSTMap<K extends Comparable<K>, V> implements Map<K, V> {
+
+    private class Node {
+        public K key;
+        public V value;
+        public Node left;
+        public Node right;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public BSTMap() {
+        root = null;
+        size = 0;
+    }
+
+    /**
+     * 添加元素
+     *
+     * @param key
+     * @param value
+     */
+    @Override
+    public void add(K key, V value) {
+        root = add(root, key, value);
+    }
+
+    private Node add(Node node, K key, V value) {
+        if (node == null) {
+            size++;
+            return new Node(key, value);
+        }
+        if (key.compareTo(node.key) < 0) {
+            node.left = add(node.left, key, value);
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = add(node.right, key, value);
+        } else {
+            node.value = value;
+        }
+        return node;
+    }
+
+    private Node getNode(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+        if (key.compareTo(node.key) == 0) {
+            return node;
+        } else if (key.compareTo(node.key) < 0) {
+            return getNode(node.left, key);
+        } else {
+            return getNode(node.right, key);
+        }
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return getNode(root, key) != null;
+    }
+
+    @Override
+    public V get(K key) {
+        Node node = getNode(root, key);
+        return node == null ? null : node.value;
+    }
+
+    @Override
+    public void set(K key, V newValue) {
+        Node node = getNode(root, key);
+        if (node == null) {
+            throw new IllegalArgumentException(key + " doesn't exist!");
+        }
+        node.value = newValue;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 返回以node为根的二分搜索树的最小值所在的节点
+     *
+     * @param node
+     * @return
+     */
+    private Node minNum(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return minNum(node.left);
+    }
+
+    /**
+     * 删除掉以node为根的而分搜索树中的最小节点
+     * 返回删除节点后新的二分搜索树对的根
+     *
+     * @param node
+     * @return
+     */
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    /**
+     * 从二分搜索树中删除键为key的节点
+     *
+     * @param key
+     */
+    @Override
+    public V remove(K key) {
+        Node node = getNode(root, key);
+        if (node == null) {
+            return null;
+        }
+        root = remove(root, key);
+        return node.value;
+    }
+
+    /**
+     * 删除掉以node为根的二分搜索树中值为key的节点，递归算法
+     * 返回删除节点后新的二分搜索树的根
+     *
+     * @param node
+     * @param key
+     * @return
+     */
+    private Node remove(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+        if (key.compareTo(node.key) < 0) {
+            node.left = remove(node.left, key);
+            return node;
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = remove(node.right, key);
+            return node;
+        } else {
+            //待删除节点左子树为空的情况
+            if (node.left == null) {
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+            //待删除节点右子树为空的情况
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return leftNode;
+            }
+            //待删除节点左右子树都不为空的情况
+
+            //找到比待删除节点大的最小节点的，即待删除结点右子树的最小节点
+            //用这个节点顶替待删除节点的位置
+            Node successor = minNum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+            node.left = node.right = null;
+            return successor;
+        }
+    }
+}
