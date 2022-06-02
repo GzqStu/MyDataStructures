521d2780a0f5b446a85532d10be17687b28a8266
DESKTOP-0LUK01P\Mr.Gao <2538120593@qq.com>
2022-06-02 14:41:36 +0800

    实现优先队列

---
+++ src/com/gao/cn/queue/PriorityQueue.java
@@ -0,0 +1,42 @@
+package com.gao.cn.queue;
+
+import com.gao.cn.heap.MaxHeap;
+
+/**
+ * 优先队列
+ * @author ：gaozhiqi
+ * @date ：2022/6/2 14:34
+ */
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+
+    private MaxHeap<E> maxHeap;
+
+    public PriorityQueue(){
+        maxHeap = new MaxHeap<>();
+    }
+
+    @Override
+    public int getSize() {
+        return maxHeap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return maxHeap.isEmpty();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        maxHeap.add(e);
+    }
+
+    @Override
+    public E deQueue() {
+        return maxHeap.extractMax();
+    }
+
+    @Override
+    public E getFront() {
+        return maxHeap.findMax();
+    }
+}
