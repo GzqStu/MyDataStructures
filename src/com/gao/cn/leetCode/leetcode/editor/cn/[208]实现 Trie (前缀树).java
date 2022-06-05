5bafb38141a10f5af711bafd01662ae8f852224c
DESKTOP-0LUK01P\Mr.Gao <2538120593@qq.com>
2022-06-05 15:45:44 +0800

    解决leetcode 208题

---
+++ src/com/gao/cn/leetCode/leetcode/editor/cn/[208]实现 Trie (前缀树).java
@@ -0,0 +1,119 @@
+package com.gao.cn.leetCode.leetcode.editor.cn;//Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补完和拼
+//写检查。 
+//
+// 请你实现 Trie 类： 
+//
+// 
+// Trie() 初始化前缀树对象。 
+// void insert(String word) 向前缀树中插入字符串 word 。 
+// boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 
+//false 。 
+// boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否
+//则，返回 false 。 
+// 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入
+//["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+//[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+//输出
+//[null, null, true, false, true, null, true]
+//
+//解释
+//Trie trie = new Trie();
+//trie.insert("apple");
+//trie.search("apple");   // 返回 True
+//trie.search("app");     // 返回 False
+//trie.startsWith("app"); // 返回 True
+//trie.insert("app");
+//trie.search("app");     // 返回 True
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= word.length, prefix.length <= 2000 
+// word 和 prefix 仅由小写英文字母组成 
+// insert、search 和 startsWith 调用次数 总计 不超过 3 * 10⁴ 次 
+// 
+// Related Topics 设计 字典树 哈希表 字符串 👍 1198 👎 0
+
+
+import java.util.TreeMap;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Trie {
+    private class Node {
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            this.next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+    private Node root;
+    public Trie() {
+        root = new Node();
+    }
+
+    public void insert(String word) {
+        Node cur = root;
+        for(int i = 0;i<word.length();i++){
+            char c  = word.charAt(i);
+            if(cur.next.get(c) == null){
+                cur = cur.next.put(c,new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        cur.isWord = true;
+    }
+
+    public boolean search(String word) {
+        Node cur = root;
+        for(int i = 0;i<word.length();i++){
+            char c  = word.charAt(i);
+            if(cur.next.get(c) == null){
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return cur.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        Node cur = root;
+        for(int i = 0;i<prefix.length();i++){
+            char c  = prefix.charAt(i);
+            if(cur.next.get(c) == null){
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Trie obj = new Trie();
+        obj.insert("abdddddddwwwwwwwwiuokljmnbb");
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
--- src/com/gao/cn/trie/Trie.java
+++ src/com/gao/cn/trie/Trie.java
@@ -72,4 +72,21 @@ public class Trie {
         }
         return cur.isWord;
     }
+
+    /**
+     * 查询是否在Trie中有单词以prefix为前缀
+     * @param prefix
+     * @return
+     */
+    public boolean isPrefix(String prefix){
+        Node cur = root;
+        for(int i = 0;i<prefix.length();i++){
+            char c = prefix.charAt(i);
+            if(cur.next.get(c)==null){
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return true;
+    }
 }
