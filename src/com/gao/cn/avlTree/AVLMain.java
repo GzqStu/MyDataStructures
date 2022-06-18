a2ac20fcebd4163a68fd4859a3c0b0307de6996b
DESKTOP-0LUK01P\Mr.Gao <2538120593@qq.com>
2022-06-18 18:21:16 +0800

    实现AVL树的添加，删除，以及平衡维护的功能

--- src/com/gao/cn/avlTree/AVLMain.java
+++ src/com/gao/cn/avlTree/AVLMain.java
@@ -1,6 +1,6 @@
 package com.gao.cn.avlTree;
 
-import com.gao.cn.set.BSTSet;
+import com.gao.cn.map.BSTMap;
 import com.gao.cn.util.FileOperation;
 
 import java.util.ArrayList;
@@ -11,24 +11,94 @@ import java.util.ArrayList;
  */
 public class AVLMain {
 
-    public static void main(String[] args) {
+    /**
+     * 测试AVL树，是否是二分搜索树，以及平衡二叉树
+     */
+    private static void testIsAVLTree() {
         System.out.println("Pride and Prejudice");
         ArrayList<String> words = new ArrayList<>();
         if (FileOperation.readFile("pride-and-prejudice.txt", words)) {
-            System.out.println("Total words: "+words.size());
-            AVLTree<String,Integer> map = new AVLTree<>();
-            long startTime = System.nanoTime();
-            BSTSet<String> set = new BSTSet<>();
+            System.out.println("Total words: " + words.size());
+            AVLTree<String, Integer> map = new AVLTree<>();
             for (String word : words) {
-                set.add(word);
+                if (map.contains(word)) {
+                    map.set(word, map.get(word) + 1);
+                } else {
+                    map.add(word, 1);
+                }
+            }
+            System.out.println("is BST : " + map.isBST());
+            System.out.println("is Balanced : " + map.isBalanced());
+            for(String word:words){
+                map.remove(word);
+                if(!map.isBST() || !map.isBalanced()){
+                    throw new RuntimeException("Error");
+                }
+            }
+        }
+        System.out.println("AVLTree designer is success!");
+    }
+
+    /**
+     * 测试二分搜索树性能
+     */
+    private static void testBST() {
+        System.out.println("Pride and Prejudice");
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile("pride-and-prejudice.txt", words)) {
+            System.out.println("Total words: " + words.size());
+            //Test BST
+            long startTime = System.nanoTime();
+            BSTMap<String, Integer> bst = new BSTMap<>();
+            for (String word : words) {
+                if (bst.contains(word)) {
+                    bst.set(word, bst.get(word) + 1);
+                } else {
+                    bst.add(word, 1);
+                }
             }
             for (String word : words) {
-                set.contains(word);
+                bst.contains(word);
             }
             long endTime = System.nanoTime();
             double time = (endTime - startTime) / 1000000000.0;
-            System.out.println("Total different words: " + set.getSize());
-            System.out.println("BSTSet: " + time + "s");
+            System.out.println("BST : " + time + " s");
         }
+    }
+
+    /**
+     * 测试AVL树性能
+     */
+    private static void testAVLTree() {
+        System.out.println("Pride and Prejudice");
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile("pride-and-prejudice.txt", words)) {
+            System.out.println("Total words: " + words.size());
+            //Test BST
+            long startTime = System.nanoTime();
+            AVLTree<String, Integer> bst = new AVLTree<>();
+            for (String word : words) {
+                if (bst.contains(word)) {
+                    bst.set(word, bst.get(word) + 1);
+                } else {
+                    bst.add(word, 1);
+                }
+            }
+            for (String word : words) {
+                bst.contains(word);
+            }
+            long endTime = System.nanoTime();
+            double time = (endTime - startTime) / 1000000000.0;
+            System.out.println("AVLTree : " + time + " s");
+        }
+    }
+
+    public static void main(String[] args) {
+        //测试AVL树，是否是二分搜索树，以及平衡二叉树
+        testIsAVLTree();
+        //测试二分搜索树性能
+        /*testAVLTree();*/
+        //测试二分搜索树性能
+        /*testBST();*/
     }
 }
--- src/com/gao/cn/avlTree/AVLTree.java
+++ src/com/gao/cn/avlTree/AVLTree.java
@@ -43,6 +43,58 @@ public class AVLTree<K extends Comparable<K>, V> {
         return size == 0;
     }
 
+    /**
+     * 判断该二叉树是否是一棵二分搜索树
+     *
+     * @return
+     */
+    public boolean isBST() {
+        ArrayList<K> keys = new ArrayList<>();
+        inOrder(root, keys);
+        for (int i = 1; i < keys.size(); i++) {
+            if (keys.get(i - 1).compareTo(keys.get(i)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void inOrder(Node node, ArrayList<K> keys) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left, keys);
+        keys.add(node.key);
+        inOrder(node.right, keys);
+
+    }
+
+    /**
+     * 判断该二叉树是否是一棵平衡二叉树
+     *
+     * @return
+     */
+    public boolean isBalanced() {
+        return isBalanced(root);
+    }
+
+    /**
+     * 判断以Node为根的二叉树是否是一棵平衡二叉树，递归算法
+     *
+     * @param node
+     * @return
+     */
+    private boolean isBalanced(Node node) {
+        if (node == null) {
+            return true;
+        }
+        int balanceFactor = getBalanceFactor(node);
+        if (Math.abs(balanceFactor) > 1) {
+            return false;
+        }
+        return isBalanced(node.left) && isBalanced(node.right);
+    }
+
     // 获得节点node的高度
     private int getHeight(Node node) {
         if (node == null)
@@ -55,6 +107,83 @@ public class AVLTree<K extends Comparable<K>, V> {
         if (node == null)
             return 0;
         return getHeight(node.left) - getHeight(node.right);
+    }
+
+    // 对节点y进行向右旋转操作，返回旋转后新的根节点x
+    //        y                              x
+    //       / \                           /   \
+    //      x   T4     向右旋转 (y)        z     y
+    //     / \       - - - - - - - ->    / \   / \
+    //    z   T3                       T1  T2 T3 T4
+    //   / \
+    // T1   T2
+    private Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T3 = x.right;
+        //向右旋转过程
+        x.right = y;
+        y.left = T3;
+        //更新height
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+        return x;
+    }
+
+    // 对节点y进行向左旋转操作，返回旋转后新的根节点x
+    //    y                             x
+    //  /  \                          /   \
+    // T1   x      向左旋转 (y)       y     z
+    //     / \   - - - - - - - ->   / \   / \
+    //   T2  z                     T1 T2 T3 T4
+    //      / \
+    //     T3 T4
+    private Node leftRotate(Node y) {
+        Node x = y.right;
+        Node T2 = x.left;
+
+        // 向左旋转过程
+        x.left = y;
+        y.right = T2;
+
+        // 更新height
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+
+        return x;
+    }
+
+    /**
+     * 维护AVL树的高度和平衡
+     *
+     * @param node
+     * @return
+     */
+    private Node balanceMaintain(Node node) {
+        // 更新height
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+
+        // 计算平衡因子
+        int balanceFactor = getBalanceFactor(node);
+        //平衡维护
+        //LL
+        if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0) {
+            return rightRotate(node);
+        }
+        //RR
+        if (balanceFactor < -1 && getBalanceFactor(node.right) <= 0) {
+            return leftRotate(node);
+        }
+        //LR
+        if (balanceFactor > 1 && getBalanceFactor(node.left) < 0) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+        //RL
+        if (balanceFactor < -1 && getBalanceFactor(node.right) > 0) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+        return node;
     }
 
     // 向二分搜索树中添加新的元素(key, value)
@@ -77,16 +206,8 @@ public class AVLTree<K extends Comparable<K>, V> {
             node.right = add(node.right, key, value);
         else // key.compareTo(node.key) == 0
             node.value = value;
-
-        // 更新height
-        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
-
-        // 计算平衡因子
-        int balanceFactor = getBalanceFactor(node);
-        if (Math.abs(balanceFactor) > 1)
-            System.out.println("unbalanced : " + balanceFactor);
-
-        return node;
+        //维护高度和平衡
+        return balanceMaintain(node);
     }
 
     // 返回以node为根节点的二分搜索树中，key所在的节点
@@ -158,43 +279,42 @@ public class AVLTree<K extends Comparable<K>, V> {
 
         if (node == null)
             return null;
-
+        Node retNode = null;
         if (key.compareTo(node.key) < 0) {
             node.left = remove(node.left, key);
-            return node;
+            retNode = node;
         } else if (key.compareTo(node.key) > 0) {
             node.right = remove(node.right, key);
-            return node;
+            retNode = node;
         } else {   // key.compareTo(node.key) == 0
-
             // 待删除节点左子树为空的情况
             if (node.left == null) {
                 Node rightNode = node.right;
                 node.right = null;
                 size--;
-                return rightNode;
-            }
-
-            // 待删除节点右子树为空的情况
-            if (node.right == null) {
+                retNode = rightNode;
+            } else if (node.right == null) {
+                // 待删除节点右子树为空的情况
                 Node leftNode = node.left;
                 node.left = null;
                 size--;
-                return leftNode;
+                retNode = leftNode;
+            } else {
+                // 待删除节点左右子树均不为空的情况
+                // 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
+                // 用这个节点顶替待删除节点的位置
+                Node successor = minimum(node.right);
+                successor.right = remove(node.right, successor.key);
+                successor.left = node.left;
+                node.left = node.right = null;
+                retNode = successor;
             }
-
-            // 待删除节点左右子树均不为空的情况
-
-            // 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
-            // 用这个节点顶替待删除节点的位置
-            Node successor = minimum(node.right);
-            successor.right = removeMin(node.right);
-            successor.left = node.left;
-
-            node.left = node.right = null;
-
-            return successor;
         }
+        if (retNode == null) {
+            return null;
+        }
+        //维护高度和平衡
+        return balanceMaintain(retNode);
     }
 
     public static void main(String[] args) {
